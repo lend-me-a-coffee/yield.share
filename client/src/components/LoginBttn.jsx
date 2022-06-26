3d5491af90bfdac8236175a692e5271a60ad6dfc
Javier Bullrich <javier@bullrich.dev>
2022-06-26 05:15:51 -0400

    implemented wallet provider in login button

--- client/src/components/LoginBttn.jsx
+++ client/src/components/LoginBttn.jsx
@@ -1,12 +1,10 @@
-import { useEffect, useState, useContext } from "react";
-import {
-    Button,
-} from "@chakra-ui/react";
-import {networkParams} from "../config/chainConfig"
-import { ethers } from "ethers";
-import { providerOptions } from "../config/providerOptions";
+import {useContext, useEffect, useState} from "react";
+import {Button,} from "@chakra-ui/react";
+import {ethers} from "ethers";
+import {providerOptions} from "../config/providerOptions";
 import Web3Modal from "web3modal";
-import { userContext } from "../context/UserContext";
+import {userContext} from "../context/UserContext";
+import {useWallet} from "../context/Wallet";
 
 
 const LoginBttn = () => {
@@ -15,49 +13,54 @@ const LoginBttn = () => {
         providerOptions // required
     });
 
-    const { address, setAddress } = useContext(userContext);
+    const wallet = useWallet();
+
+    const {address, setAddress} = useContext(userContext);
     const [chainId, setChainId] = useState("")
 
 
     const connectWallet = async () => {
         try {
             const provider = await web3Modal.connect();
-            const library = new ethers.providers.Web3Provider(provider);
+            const library = new ethers.providers.Web3Provider(provider, "any");
             const accounts = await library.listAccounts();
             const network = await library.getNetwork();
             if (accounts) setAddress(accounts[0]);
             setChainId(network.chainId);
-            } catch (error) {
+            const sign = library.getSigner();
+
+            wallet.setAddress(accounts[0], library.getSigner());
+        } catch (error) {
             console.log(error);
-            }
+        }
     };
-        
+
 
     const disconnect = async () => {
         await web3Modal.clearCachedProvider();
         refreshState();
     };
-    
+
 
     const refreshState = () => {
         setAddress();
         setChainId();
     };
-    
-        
+
+
     useEffect(() => {
         if (web3Modal.cachedProvider) {
             connectWallet();
         }
     }, []);
-    
+
 
     return (
         <>
             {!address ? (
-            <Button variant={"ghost"} onClick={connectWallet}>Connect Wallet</Button>
+                <Button variant={"ghost"} onClick={connectWallet}>Connect Wallet</Button>
             ) : (
-            <Button variant={"ghost"} onClick={disconnect}>Disconnect</Button>
+                <Button variant={"ghost"} onClick={disconnect}>Disconnect</Button>
             )}
         </>
     )
