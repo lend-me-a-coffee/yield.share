9fa1cf727a550eb0fed19b13ecc0d9ead49612b5
iamcryptoboi <iamcryptoboi@gmail.com>
2022-06-25 22:40:43 -0400

    add staking and unstaking

--- client/contracts/contracts/CreatorStaking.sol
+++ client/contracts/contracts/CreatorStaking.sol
@@ -2,11 +2,13 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract CreatorStaking is ERC721, ERC721Burnable, Ownable {
+
     using Counters for Counters.Counter;
 
     struct TokenData {
@@ -17,6 +19,33 @@ contract CreatorStaking is ERC721, ERC721Burnable, Ownable {
     Counters.Counter private _tokenIdCounter;
 
     mapping(uint256 => TokenData) private comments;
+
+    struct Staker {
+        address payable staker;
+        uint256 amount;
+        uint256 deadline;
+    }
+
+    mapping(uint256 => Staker) public stakers;
+
+    function stake(uint256 deadline, string memory comment) external payable returns (uint256) {
+        require(msg.value > 0, 'amount must be greater than 0');
+        uint256 id = mint(comment, msg.sender);
+        stakers[id] = Staker(payable(msg.sender), msg.value, deadline);
+        return id;
+    }
+
+    function unstake(uint256 id) external payable returns (uint256) {
+        uint256 amount = stakers[id].amount;
+        address payable staker = stakers[id].staker;
+        require(amount > 0, 'sender must have deposits greater than 0');
+        require(msg.sender == staker, 'sender must own the nft');
+        
+        stakers[id].amount = 0;
+        staker.transfer(amount);
+        
+        return amount;
+    }
 
     constructor() ERC721("Comment", "CMT") {}
 
